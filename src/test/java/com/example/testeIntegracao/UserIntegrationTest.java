8d5a74262d108caa78d3c9befbc887536189f803
Henrique Lindman Bassi <148555934+LindmanBassi@users.noreply.github.com>
2026-05-19 07:47:27 -0300

    alteracoes do cicd

--- src/test/java/com/example/testeIntegracao/UserIntegrationTest.java
+++ src/test/java/com/example/testeIntegracao/UserIntegrationTest.java
@@ -11,20 +11,22 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+// Sobe o contexto completo da aplicação (Spring, JPA, Hibernate etc.)
 @ActiveProfiles("test")
+// Garante que esta sendo usado as configurações do ambiente de teste (ex: H2)
 class UserIntegrationTest {
 
-    @Autowired
+    @Autowired // Injeta o repositório REAL (não é mock)
     private UserRepository repository;
 
     @Test
     void deveSalvarUsuarioNoBanco() {
 
-        User user = new User("Henrique");
+        User user = new User("Henrique"); // Criamos um usuario real para persistência
 
-        repository.save(user);
+        repository.save(user); //dado salvo no banco h2
 
-        List<User> usuarios = repository.findAll();
+        List<User> usuarios = repository.findAll(); //busca dados reais para validacao
 
        // assertFalse(usuarios.isEmpty());
         assertTrue(usuarios.isEmpty());
--- src/test/java/com/example/testeIntegracao/UserServiceTest.java
+++ src/test/java/com/example/testeIntegracao/UserServiceTest.java
@@ -11,24 +11,29 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+// Ativa o suporte do Mockito no JUnit 5, permitindo criar mocks automaticamente
 class UserServiceTest {
 
     @Mock
+    // Cria um mock do repository (não acessa banco real)
     private UserRepository repository;
 
     @InjectMocks
+    // Injeta automaticamente o mock dentro do UserService
+    // O service "pensa" que esta usando o repository real
     private UserService service;
 
     @Test
     void deveSalvarUsuario() {
 
-        User user = new User("Henrique");
+        User user = new User("Henrique"); // Criacao de um usuario para teste
 
-        when(repository.save(any()))
-                .thenReturn(user);
+        when(repository.save(any())) // Simulamos o comportamento do repository
+                .thenReturn(user);    // quando salvar qualquer usuario, retorna o próprio objeto criado
 
-        User salvo = service.salvar(user);
+        User salvo = service.salvar(user);  // Executa a logica real do service
 
-        assertEquals("Henrique", salvo.getNome());
+        assertEquals("Henrique", salvo.getNome());  // Verifica se a logica do service funcionou corretamente
+        //assertEquals("Thiago", salvo.getNome());
     }
 }
